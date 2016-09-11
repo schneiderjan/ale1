bda8f521a4cc6deffbe767646d902908429a1541
Jan-Niklas Schneider <heisse.cola@gmail.com>
2016-09-11 22:58:16 +0200

    gui

---
+++ AleWk1/AleWk1/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace AleWk1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnParse_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
