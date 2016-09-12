8995061a0f979bd392082a5ff038e90df7c0e72d
Jan-Niklas Schneider <heisse.cola@gmail.com>
2016-09-12 12:49:30 +0200

    Some gui updates

--- AleWk1/AleWk1/MainWindow.xaml.cs
+++ AleWk1/AleWk1/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
 
 namespace AleWk1
 {
@@ -7,14 +9,33 @@ namespace AleWk1
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        List<string> operators;
+                public MainWindow()
         {
             InitializeComponent();
+            operators = new List<string>()
+            {
+                "~",
+                "&",
+                "|",
+                ">",
+                "=",
+            };
         }
 
         private void btnParse_Click(object sender, RoutedEventArgs e)
         {
+            ParseInfix(tbPrefix.Text.Trim());
+        }
 
+        private void ParseInfix(string text)
+        {
+            GetTree(text);
+        }
+
+        private void GetTree(string text)
+        {
+            
         }
     }
 }
---
+++ AleWk1/AleWk1/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AleWk1
+{
+    public class Node
+    {
+        public Node Parent { get; set; }
+        public Node LeftChild { get; set; }
+        public Node RightChild { get; set; }
+        public string Value { get; set; }
+    }
+}
