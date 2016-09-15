db4923e6c5913cda86552096a1c341dbebd2e5ff
Jan-Niklas Schneider <heisse.cola@gmail.com>
2016-09-15 22:25:35 +0200

    Displaying graph working

--- AleWk1/AleWk1/Helper.cs
+++ AleWk1/AleWk1/Helper.cs
@@ -5,14 +5,58 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Annotations.Storage;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
 namespace AleWk1
 {
     public static class Helper
     {
-        internal static void WriteToFile(List<string> txtFileLines)
+
+        internal static List<string> operators = new List<string>()
+            {
+                "&",
+                "|",
+                ">",
+                "=",
+            };
+
+        internal const string not = "~";
+
+
+        internal static void WriteToFile(List<Node> flatList)
         {
-            using (StreamWriter sw = new StreamWriter(@"C:\Program Files (x86)\Graphviz2.38\bin\dot.dot", false))
+            List<string> txtFileLines = new List<string>();
+
+            txtFileLines.Add("graph logic {");
+            txtFileLines.Add("node[fontname = \"Arial\"]");
+
+            foreach (var node in flatList)
+            {
+                var label = string.Format("node{0} [ label = \" {1} \" ]", flatList.IndexOf(node) + 1, node.Value);
+                txtFileLines.Add(label);
+            }
+
+            foreach (var node in flatList)
+            {
+                if (node.LeftChild != null)
+                {
+                    var nodeConnection = string.Format("node{0} -- node{1}",
+                        flatList.IndexOf(node) + 1, flatList.IndexOf(node.LeftChild) + 1);
+                    txtFileLines.Add(nodeConnection);
+                }
+                if (node.RightChild != null)
+                {
+                    var nodeConnection = string.Format("node{0} -- node{1}",
+                        flatList.IndexOf(node) + 1, flatList.IndexOf(node.RightChild) + 1);
+                    txtFileLines.Add(nodeConnection);
+                }
+
+            }
+            txtFileLines.Add("}");
+
+            using (StreamWriter sw = new StreamWriter("dot.dot", false))
             {
                 foreach (var line in txtFileLines)
                 {
@@ -21,15 +65,22 @@ namespace AleWk1
             }
         }
 
-        internal static void DisplayGraph()
+        internal static void DisplayGraph(Image imgGraph)
         {
             using (var p = new Process())
             {
+                var path = AppDomain.CurrentDomain.BaseDirectory;
+                var dotPath = path + "\\dot.dot";
+                var imgPath = path + "\\dot.png";
+
                 p.StartInfo.Verb = "runas";
+                p.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
                 p.StartInfo.FileName = @"C:\Program Files (x86)\Graphviz2.38\bin\dot.exe";
-                p.StartInfo.Arguments = " -Tpng -odot.png dot.dot";
+                p.StartInfo.Arguments = " -Tpng -odot.png " + dotPath;
                 p.Start();
                 p.WaitForExit();
+
+                imgGraph.Source = new BitmapImage(new Uri(@imgPath));
             }
         }
 
@@ -47,6 +98,43 @@ namespace AleWk1
                 chars.Add(c.ToString());
             }
             return chars;
+        }
+
+        internal static List<Node> GetFlatList(List<string> reversedPrefixInput)
+        {
+            List<Node> flatList = new List<Node>();
+            var stack = new Stack<Node>();
+            foreach (var c in reversedPrefixInput)
+            {
+                if (IsOperator(c))
+                {
+                    Node leftOperand = stack.Pop();
+                    Node rightOperand = stack.Pop();
+                    var node = new Node(c, leftOperand, rightOperand);
+                    flatList.Add(node);
+                    stack.Push(node);
+                }
+                else if (c.Equals(not))
+                {
+                    Node rightOperand = stack.Pop();
+                    var node = new Node(c, rightOperand);
+                    flatList.Add(node);
+                    stack.Push(node);
+                }
+                else
+                {
+                    var node = new Node(c);
+                    flatList.Add(node);
+                    stack.Push(node);
+                }
+            }
+            return flatList;
+        }
+
+        internal static bool IsOperator(string c)
+        {
+            if (operators.Contains(c)) return true;
+            return false;
         }
     }
 }
--- AleWk1/AleWk1/MainWindow.xaml.cs
+++ AleWk1/AleWk1/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
 namespace AleWk1
 {
@@ -10,21 +11,12 @@ namespace AleWk1
     /// </summary>
     public partial class MainWindow : Window
     {
-        private List<string> operators;
-        private const string not = "~";
-
-
         public MainWindow()
         {
             InitializeComponent();
-            operators = new List<string>()
-            {
-                "&",
-                "|",
-                ">",
-                "=",
-            };
-            tbPrefix.Text = "&(=(A,B),|(~C,D))";
+            
+            
+            tbPrefix.Text = "=( >(A,B), |( ~(A) ,B) )";
             //"=( >(A,B), |( ~(A) ,B) )
             //&(A, ~(B))
             //&(=(A,B),|(C,D))
@@ -48,90 +40,26 @@ namespace AleWk1
 
         private void ParseInfix(List<string> reversedPrefixInput)
         {
-            var flatList = GetTree(reversedPrefixInput);
-            List<Node> infixs = new List<Node>();
-            var output = "";
+            var flatList = Helper.GetFlatList(reversedPrefixInput);
 
-            GenerateGraph(flatList);
+            Helper.WriteToFile(flatList);
+            Helper.DisplayGraph(imgGraph);
+            //imgGraph.Source = new BitmapImage(new Uri("dot.png", UriKind.Relative));
+            //D:\Code\GitHub\4_fourth_year_block1\ale1\AleWk1\AleWk1\bin\Debug
 
-            foreach (var i in infixs)
-            {
-                output += i.Value;
-            }
-            tbInfix.Text = output;
+
+            //List<Node> infixs = new List<Node>();
+            //var output = "";
+            //foreach (var i in infixs)
+            //{
+            //    output += i.Value;
+            //}
+            //tbInfix.Text = output;
         }
 
-        private void GenerateGraph(List<Node> flatList)
-        {
-            List<string> txtFileLines = new List<string>();
+        
 
-            txtFileLines.Add("graph logic {");
-            txtFileLines.Add("node[fontname = \"Arial\"]");
-
-            foreach (var node in flatList)
-            {
-                var label = string.Format("node{0} [ label = \" {1} \" ]", flatList.IndexOf(node) + 1, node.Value);
-                txtFileLines.Add(label);
-            }
-
-            foreach (var node in flatList)
-            {
-                if (node.LeftChild != null)
-                {
-                    var nodeConnection = string.Format("node{0} -- node{1}",
-                        flatList.IndexOf(node) + 1, flatList.IndexOf(node.LeftChild)+1);
-                    txtFileLines.Add(nodeConnection);
-                }
-                if (node.RightChild != null)
-                {
-                    var nodeConnection = string.Format("node{0} -- node{1}",
-                        flatList.IndexOf(node) + 1, flatList.IndexOf(node.RightChild)+1);
-                    txtFileLines.Add(nodeConnection);
-                }
-            }
-
-            txtFileLines.Add("}");
-            Helper.WriteToFile(txtFileLines);
-            Helper.DisplayGraph();
-
-        }
-
-        private List<Node> GetTree(List<string> reversedPrefixInput)
-        {
-            List<Node> flatList = new List<Node>();
-            var stack = new Stack<Node>();
-            foreach (var c in reversedPrefixInput)
-            {
-                if (IsOperator(c))
-                {
-                    Node leftOperand = stack.Pop();
-                    Node rightOperand = stack.Pop();
-                    var node = new Node(c, leftOperand, rightOperand);
-                    flatList.Add(node);
-                    stack.Push(node);
-                }
-                else if (c.Equals(not))
-                {
-                    Node rightOperand = stack.Pop();
-                    var node = new Node(c, rightOperand);
-                    flatList.Add(node);
-                    stack.Push(node);
-                }
-                else
-                {
-                    var node = new Node(c);
-                    flatList.Add(node);
-                    stack.Push(node);
-                }
-            }
-            return flatList;
-        }
-
-        private bool IsOperator(string c)
-        {
-            if (operators.Contains(c)) return true;
-            return false;
-        }
+       
     }
 }
 
