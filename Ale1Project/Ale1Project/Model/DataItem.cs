94b16539891f67166930fbd905523b62b81eb486
Jan-Niklas Schneider <heisse.cola@gmail.com>
2017-02-18 20:43:19 +0100

    Created new project

---
+++ Ale1Project/Ale1Project/Model/DataItem.cs
@@ -0,0 +1,16 @@
+﻿namespace Ale1Project.Model
+{
+    public class DataItem
+    {
+        public string Title
+        {
+            get;
+            private set;
+        }
+
+        public DataItem(string title)
+        {
+            Title = title;
+        }
+    }
+}
